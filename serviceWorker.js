9c13571476bc6fb49d8528926e3dd28b03ee275b
tkntkn <kd1049@gmail.com>
2023-01-23 02:08:24 +0900

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,9 +1,9 @@
 const CACHE_NAME = "pwa-gmail-cache";
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll([])))
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll([])))
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(event.request).then(response => response ?? fetch(e.request)))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then(response => response ?? fetch(event.request)))
 });
